d79c537190b3400fd17a1efbad65a205f19e967a
Marvyn Bornemann <m.bornemann@campus.tu-berlin.de>
2022-11-25 09:49:03 +0100

    added perf-tools

--- debug_cluster_MKL.sh
+++ debug_cluster_MKL.sh
@@ -9,7 +9,6 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=debug_cluster_MKL
-#SBATCH --workdir=/p/tmp/borneman
 #SBATCH --time=15:00
 
 echo "------------------------------------------------------------"
@@ -18,5 +17,7 @@ echo "$SLURM_NTASKS tasks"
 echo "$SLURM_CPUS_PER_TASK cpus per task"
 echo "------------------------------------------------------------"
 
+export PATH=$PATH:/p/system/packages/perf-tools/bin
+
 module load julia/1.8.2
-julia --threads=$SLURM_CPUS_PER_TASK /home/borneman/Masterarbeit/debug_pik_cluster/debug_cluster.jl MKL
+perf record julia --threads=$SLURM_CPUS_PER_TASK /home/borneman/Masterarbeit/debug_pik_cluster/debug_cluster.jl MKL
